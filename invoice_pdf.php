274253e36ad8ec299ff0f2328021e39ad41104f0
Raiqa Rasool <86972954+RaiqaRasool@users.noreply.github.com>
2022-08-30 23:26:25 +0500

    pdf design changed

--- invoice_pdf.php
+++ invoice_pdf.php
@@ -1,63 +1,58 @@
 <?php
-$logo=base64_encode(file_get_contents("./assets/imgs/logo.png"));
-$mobile=base64_encode(file_get_contents("./assets/imgs/mobile.png"));
-$email=base64_encode(file_get_contents("./assets/imgs/email.png"));
-$address_glob=base64_encode(file_get_contents("./assets/imgs/address_glob.png"));
-$heart=base64_encode(file_get_contents("./assets/imgs/heart.png"));
-$sign=base64_encode(file_get_contents("./assets/imgs/sign.png"));
-$footer=base64_encode(file_get_contents("./assets/imgs/footer.png"));
+$logo = base64_encode(file_get_contents("./assets/imgs/logo.png"));
+$mobile = base64_encode(file_get_contents("./assets/imgs/mobile.png"));
+$email = base64_encode(file_get_contents("./assets/imgs/email.png"));
+$address_glob = base64_encode(file_get_contents("./assets/imgs/address_glob.png"));
+$heart = base64_encode(file_get_contents("./assets/imgs/heart.png"));
+$sign = base64_encode(file_get_contents("./assets/imgs/sign.jpg"));
+$footer = base64_encode(file_get_contents("./assets/imgs/footer.png"));
 
 
 require_once("./app/Invoice.php");
-$invoice_id=$_GET["invoice_id"];
-$invoice=new Invoice();
-$invoice_data=$invoice->get_invoice($invoice_id);
+$invoice_id = $_GET["invoice_id"];
+$invoice = new Invoice();
+$invoice_data = $invoice->get_invoice($invoice_id);
 
-$invoice_date=strtotime($invoice_data["invoice_date"]);
-$due_date=strtotime($invoice_data["invoice_due_date"]);
-$total_bill=0;
-function items_data(){
+$invoice_date = strtotime($invoice_data["invoice_date"]);
+$due_date = strtotime($invoice_data["invoice_due_date"]);
+$total_bill = 0;
+function items_data()
+{
     global $invoice;
     global $invoice_id;
     global $total_bill;
-    $invoice_items_data=$invoice->get_invoice_item($invoice_id);
-    $string="";
-   foreach($invoice_items_data as $item){
-    $string.=
-    '<tr>
+    $invoice_items_data = $invoice->get_invoice_item($invoice_id);
+    $string = "";
+    foreach ($invoice_items_data as $item) {
+        $string .=
+            '<tr>
         <td class="desc">
-        '.$item[2].'
+        ' . $item[2] . '
         </td>
         <td class="hr">
-        '.$item[3].'
+        ' . $item[3] . '
         </td>
         <td class="hr_rate">
-        '.$item[4].'
+        ' . $item[4] . '
         </td>
         <td class="amount">
-        $'.$item[5].'
+        $' . $item[5] . '
         </td>
     </tr>
     ';
-    $total_bill+=intval($item[5]);
-   }
-   return $string;
-}   
-?>
+        $total_bill += intval($item[5]);
+    }
+    return $string;
+}
 
-
-<?php
 require_once("./dompdf/vendor/autoload.php");
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 use Dompdf\Options;
 // instantiate and use the dompdf class
-$options=new Options();
+$options = new Options();
 $options->set('defaultFont', 'Helvetica');
 $dompdf = new DOMPDF($options);
-//setting custom width and height for paper
-$customPaper = array(0,0,950, 1329);
-$dompdf->setPaper($customPaper);
 $dompdf->loadHtml('<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -72,6 +67,17 @@ $dompdf->loadHtml('<!DOCTYPE html>
         line-height:100%;
 
     }
+    *{
+        padding:0px;
+        margin:0px;
+        box-sizing:border-box;
+    }
+    .header .logo img{
+        width:60%;
+    }
+    .header{
+        vertical-align:top;
+    }
     .header .logo{
         width:6.7818cm;
         height:1.9941645833cm;
@@ -82,18 +88,23 @@ $dompdf->loadHtml('<!DOCTYPE html>
     }
     .header .contacts-list{
         text-align:right;  
+        float:right;
+        display:flex;
+        line-height:65%;
     }
     .header .contacts-list .contact{
-        margin-bottom:8px;
+        text-align:center;
+        vertical-align:middle;
     }
-    .header .contacts-list .website{
-        margin-right:0.18cm;
+    .header .contacts-list .contact .text{
+        font-size:11px;
     }
-    .header .contacts-list .contact .icon{
-        margin-top:10cm;
+    .header .contacts-list .contact .icon img{
+        width:50%;
+        margin-top:1px;
     }
     .pdf-container{
-        padding:87px 136px;
+        padding:50px;
         margin:0cm;
         position:relative;
     }
@@ -102,17 +113,20 @@ $dompdf->loadHtml('<!DOCTYPE html>
         margin:0cm;
     }
     .hero-section{
-        margin-top:2.4341666667cm;
+        margin-top:20px;
+        font-size:12px;
     }
     .hero-section .right table{
         text-align:right;
         float:right;
-        margin-top:11px;
+        margin-top:5px;
+    }
+    .hero-section .left{
+        padding:0px;
     }
     .hero-section .left .invoice_date{
         margin-bottom:0.4519083333cm;
         font-weight:900;
-        font-size:12px;
     }
     .hero-section .left .godesign_address{
         line-height:140%;
@@ -120,143 +134,172 @@ $dompdf->loadHtml('<!DOCTYPE html>
     .hero-section .right .heading{
         color:#233269;
         font-weight:900;
-        font-size: 18.6667px;
-        padding-bottom:13.84px;
+        font-size:18.667px;
+        padding-bottom:8px;
         border-bottom: 5.09px solid #233269;
         width: 187.81px;
     }
     .hero-section .right table tr .invoice_no{
         margin-left:44.7%;
         font-weight:700;
-        font-size:12px;
-        padding-top:19px;
+        padding-top:10px;
         width: 187.81px;
         height:12px;
     }
     .client_details{
-        margin-top:69.97px;
+        margin-top:20px;
+        font-size:12px;
+    }
+    .client_details tr td{
+    padding:0px;
     }
     .client_details .heading td{
         font-weight: 900;
-        font-size: 12px;
         padding-bottom:10px;
     }
     .client_details .client_company td{
         padding-bottom:10px;
     }
     .invoice_details{
-        margin-top:44px;
+        margin-top:40px;
+        font-size:11px;
     }
     .invoice_details thead{
         border-top:2px solid black;
         border-bottom:2px solid black;
         text-transform:uppercase;
         font-weight:900;
-        font-size:12px;
     }
     .invoice_details thead tr th{
         text-align:center;
         padding:11px 0px;
+        font-size:12px;
     }
     .invoice_details thead tr .desc{
-        width: 500.69px;
+        width: 400px;
         text-align:left;
     }
       .invoice_details tbody tr .desc{
         text-align:left;
-        width: 500.69px;
+        width: 400px;
     }
     .invoice_details tbody tr td{
-        text-align:center;
+        padding:8px 0px 0px 0px;
         vertical-align:center;
-        padding:20px 20px 0px 0px;
+        text-align:center;
+    }
+    .invoice_details tbody tr .amount{
+        text-align:right;
+        padding-right:10px;
+    }
+    .invoice_details thead tr .amount_head{
+        text-align:right;
+        padding-right:10px;
     }
     .invoice_bottom{
-        margin-top:100px;
-    }
-    .invoice_bottom_head tr .due_date{
-        padding-left:16px;
+        left:50px;
+        right:50px;
+        width:700px;
+        bottom:410px;
+        position:absolute;
     }
     .invoice_bottom_head tr th{
         font-size:12px;
         font-weight:900;
     }
-    .invoice_bottom tbody tr .due_date{
-        padding-top:15px;
-
-    }
-    .invoice_bottom thead,tbody tr td,th{
-        padding-left:20px;
-        vertical-align:center;
-    }
     .invoice_bottom .invoice_bottom_body tr .thanks{
         vertical-align:top;
-         padding-top:12px;
-         width:450px;
+         padding-left:0px;
     }
-    .invoice_bottom invoice_bottom_head tr .thanks{
-        width:450px; 
+    .invoice_bottom .invoice_bottom_head .total_bill{
+        text-align:right;
+        padding-right:10px;
     }
-    .invoice_bottom .invoice_bottom_body tr .thanks .sub_table tr td{
-         vertical-align:top;
-         padding-left:10px;
+    .invoice_bottom .invoice_bottom_body tr .due_date{
+        font-size:20px;
+        width:200px;
+        padding-left:20px; 
     }
-    .invoice_bottom thead{
-         border-bottom:1px solid #000;
+    .invoice_bottom .invoice_bottom_body tr .total_bill{
+        text-align:right;
+        padding-right:10px;
+        font-size:20px;
+        width:100px;
     }
-    .invoice_bottom thead tr th{
+    .invoice_bottom .invoice_bottom_body tr .thanks .sub_table tr .text{
+        padding-left:10px;
+        line-height:110%;
+        font-size:12px;
+        width:270px;
+    }
+    .invoice_bottom .invoice_bottom_body tr .thanks .sub_table tr .heart{
+         padding-left:0px;
+         vertical-align:middle;
+    }
+    .invoice_bottom .invoice_bottom_head{
+            border-bottom:1px solid black;
+    }
+    .invoice_bottom .invoice_bottom_head tr th{
         text-align:left;
-        padding:17px 0px;
+        padding:5px 0px;
     }
-    .invoice_bottom thead tr .total_bill{
-        text-align:center;
+    .invoice_bottom .invoice_bottom_body{
+        vertical-align:middle;
     }
-    .invoice_bottom tbody tr .total_bill{
-        text-align:center;
-        padding-top:15px;
-    }
-    .invoice_bottom .invoice_bottom_body tr td{
+    .invoice_bottom .invoice_bottom_body tr .main_table_cell{
        font-weight: 900;
-        font-size: 18.6667px;
+        padding-left:0px;
+        padding-top:5px;
     }
-
     .account_details{
-        margin-top:100px;
+        font-size:11px;
+        left:50px;
+        right:50px;
+        width:700px;
+        bottom:290px;
+        position:absolute;
     }
     .account_details .account_head{
         border-bottom:1px solid black;
-        padding-bottom:16px;
+        padding-bottom:2px;
     }
     .account_details .account_head tr th{
         text-align:left;
     }
     .account_details .account_body tr td{
-        padding-top:36px;
-        font-size:18px;
+        padding-top:10px;
         line-height:120%;
-    }
-    .account_details .account_body tr .right_account{
-        text-align:right;
     }
     .account_details .account_body tr .right_account div{
        float:right;
+       padding-right:40px;
     }
 
     .sign_table{
-        margin-top:100px;
-    }
-    .sign_table{
         float:right;
+        position:absolute;
+        right:50px;
+        bottom:80px;
+        width:200px;
+    }
+    .sign_table .sign_sub{
+        width:200px;
     }
     .sign_table .sign_sub tr .text{
-        font-size:18px;
-        padding-top:20px;
+        border:none;
+        font-size:11px;
+        padding-top:5px;
         text-align:left;
         line-height:120%;
     }
+    .sign_table .sign_sub tr .sign-image img{
+        z-index:-1;
+        width:45%;
+    }
     .sign_table .sign_sub tr .sign-image{
+        border:none;
         border-bottom:1px solid black;
-        padding-bottom:20px;
+        padding-bottom:5px;
     }
 
     footer{
@@ -267,7 +310,8 @@ $dompdf->loadHtml('<!DOCTYPE html>
     }
     .registration{
         text-align:center;
-       margin-bottom:30px;
+       margin-bottom:8px;
+       font-size:16px;
     }
     .footer{
         bottom:0px;
@@ -286,38 +330,44 @@ $dompdf->loadHtml('<!DOCTYPE html>
     </td>
     </tr>
 </table>
-<img width="100%" class="footer" src="data:image;base64,'.$footer.'" alt="footer"/>
+<img width="100%" class="footer" src="data:image;base64,' . $footer . '" alt="footer"/>
 </footer>
     <div class="pdf-container">
         <table class="header" width="100%">
             <td class="logo">
-                <img src="data:image;base64,'.$logo.'" alt="logo"/>
+                <img src="data:image;base64,' . $logo . '" alt="logo"/>
             </td>
             <td class="contacts-list">
-                <div class="contact">
-                    <span class="icon">
-                        <img src="data:image;base64,'.$mobile.'" alt="mobile"/>
-                    </span>
-                    <span class="text">
-                        +92(307) 607-7533
-                    </span>
-                </div>
-                <div class="contact">
-                    <span class="icon">
-                        <img src="data:image;base64,'.$email.'" alt="mobile"/>
-                    </span>
-                    <span class="text">
-                        info@godesign.pk
-                    </span>
-                </div>
-                <div class="contact website">
-                    <span class="icon">
-                        <img src="data:image;base64,'.$address_glob.'" alt="mobile"/>
-                    </span>
-                    <span class="text">
-                        www.godesign.pk
-                    </span>
-                </div>
+                <table class="contact">
+                    <tr>
+                        <td class="icon">
+                            <img src="data:image;base64,' . $mobile . '" alt="mobile"/>
+                        </td>
+                        <td class="text">
+                            +92(307) 607-7533
+                        </td>
+                    </tr>
+                </table>
+                <table class="contact">
+                    <tr>
+                        <td class="icon">
+                            <img src="data:image;base64,' . $email . '" alt="mobile"/>
+                        </td>
+                        <td class="text">
+                            info@godesign.pk
+                        </td>
+                    </tr>
+                </table>
+                <table class="contact website">
+                    <tr>
+                        <td class="icon">
+                            <img src="data:image;base64,' . $address_glob . '" alt="mobile"/>
+                        </td>
+                        <td class="text">
+                            www.godesign.pk
+                        </td>
+                    </tr>
+                </table>
             </td>
             </tr>
         </table>   
@@ -325,7 +375,7 @@ $dompdf->loadHtml('<!DOCTYPE html>
         <table class="hero-section" width="100%">
         <tr>
             <td class="left">
-                <div class="invoice_date">'.date("d F, Y",$invoice_date).'</div>
+                <div class="invoice_date">' . date("d F, Y", $invoice_date) . '</div>
                 <div class="godesign_address">
                     Street 25, SEC-I-10/4 I-10,<br/>
                     Islamabad Capital Territory <br/>
@@ -341,7 +391,7 @@ $dompdf->loadHtml('<!DOCTYPE html>
             </tr>
             <tr>
                 <td class="invoice_no">
-                    Invoice No. GD-'.(($invoice_id%2000)+2000).'
+                    Invoice No. GD-' . (($invoice_id % 2000) + 2000) . '
                 </td>
             </tr>
             <table>
@@ -353,19 +403,19 @@ $dompdf->loadHtml('<!DOCTYPE html>
             <td>Bill To</td>
             </tr>
             <tr class="client_name">
-            <td>'.$invoice_data["invoice_receiver_name"].'</td>
+            <td>' . $invoice_data["invoice_receiver_name"] . '</td>
             </tr>
             <tr class="client_company">
-            <td>'.$invoice_data["invoice_receiver_company"].'</td>
+            <td>' . $invoice_data["invoice_receiver_company"] . '</td>
             </tr>
             <tr class="client_street">
-            <td>'.$invoice_data["invoice_receiver_street"].'</td>
+            <td>' . $invoice_data["invoice_receiver_street"] . '</td>
             </tr>
             <tr class="client_address">
-            <td>'.$invoice_data["invoice_receiver_city"].', '.$invoice_data["invoice_receiver_state"].', '.$invoice_data["invoice_receiver_country"].'</td>
+            <td>' . $invoice_data["invoice_receiver_city"] . ', ' . $invoice_data["invoice_receiver_state"] . ', ' . $invoice_data["invoice_receiver_country"] . '</td>
             </tr>
             <tr class="client_zip">
-            <td>'.$invoice_data["invoice_receiver_zip"].'</td>
+            <td>' . $invoice_data["invoice_receiver_zip"] . '</td>
             </tr>
         </table>
         
@@ -373,13 +423,13 @@ $dompdf->loadHtml('<!DOCTYPE html>
             <thead>
                 <tr>
                     <th class="desc">Description</th>
-                    <th>Hours</th>
-                    <th>Hourly Rate</th>
-                    <th>Amount</th>
+                    <th class="hrs_head">Hours</th>
+                    <th class="hr_rate_head">Hourly Rate</th>
+                    <th class="amount_head">Amount</th>
                 </tr>
             </thead>
             <tbody>
-                '.items_data().'
+                ' . items_data() . '
             </tbody>
         </table>
         <table class="invoice_bottom" width="100%">
@@ -397,11 +447,11 @@ $dompdf->loadHtml('<!DOCTYPE html>
             </thead>
             <tbody class="invoice_bottom_body">
                 <tr>
-                    <td class="thanks">
+                    <td class="main_table_cell thanks">
                         <table class="sub_table">
                             <tr>
                                 <td class="heart">
-                                    <img width="20" src="data:image;base64,'.$heart.'" alt="heart"/>
+                                    <img width="35" src="data:image;base64,' . $heart . '" alt="heart"/>
                                 </td>
                                 <td class="text">
                                     THANK YOU FOR YOUR BUSINESS. PLEASE DEPOSIT PAYMENT BY DUE DATE
@@ -409,12 +459,12 @@ $dompdf->loadHtml('<!DOCTYPE html>
                             </tr>   
                         </table>
                     </td>
-                    <td class="due_date">
-                        '.date("d F Y").'
+                    <td class="main_table_cell due_date ">
+                        ' . date("d F Y") . '
                     </td>
 
-                    <td class="total_bill">
-                        $'.$total_bill.'
+                    <td class="main_table_cell total_bill">
+                        $' . $total_bill . '
                     </td>
                 </tr>
             </tbody>
@@ -432,7 +482,7 @@ $dompdf->loadHtml('<!DOCTYPE html>
             <tbody class="account_body">
             <tr>
                 <td>
-                    ACCOUNT NAME: GODESIGN TECHNOLOGIES LLP<br/>
+                    ACCOUNT NAME: <b>GODESIGN TECHNOLOGIES LLP</b><br/>
                     BANK NAME: FAYSAL BANK LIMITED<br/>
                     ADDRESS: H#373 ST. 25 SEC-I-10/4 44800,<br/>
                     ISLAMABAD,PAKISTAN
@@ -452,7 +502,7 @@ $dompdf->loadHtml('<!DOCTYPE html>
             <table class="sign_sub">
                 <tr>
                     <td class="sign-image">
-                        <img src="data:image;base64,'.$sign.'" alt="sign"/>
+                        <img src="data:image;base64,' . $sign . '" alt="sign"/>
                     </td>
                 </tr>
                 <tr>
@@ -468,14 +518,14 @@ $dompdf->loadHtml('<!DOCTYPE html>
 </body>
 </html>');
 
-// (Optional) Setup the paper size and orientation
-// $dompdf->setPaper('A4', 'portrait');
+// Setup the paper size and orientation
+$dompdf->setPaper('A4', 'portrait');
 
 // Render the HTML as PDF
 $dompdf->render();
 
 // Output the generated PDF to Browser
-if($_GET["mode"]=='d')
-$dompdf->stream("invoice_godesign.pdf",array("Attachment"=>true));
+if ($_GET["mode"] == 'd')
+    $dompdf->stream("invoice_godesign.pdf", array("Attachment" => true));
 else
-$dompdf->stream("invoice_godesign.pdf",array("Attachment"=>false));
+    $dompdf->stream("invoice_godesign.pdf", array("Attachment" => false));
